bafcee965bc8404eb8f293c9fe8fed54dc03d92a
Sarvesh-Mk <me.fast.learn@gmailcom>
2024-06-05 17:43:57 -0400

    trying to use flatList

--- app/list.js
+++ app/list.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, SafeAreaView, View, Text, FlatList } from 'react-native';
 import { useState, useEffect } from 'react';
 import { Link, useRouter } from 'expo-router';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -12,8 +12,11 @@ export default function listPage() {
   const fetchData = async () => {
     const keys = await AsyncStorage.getAllKeys();
     const data = await AsyncStorage.multiGet(keys);
-    setItems(data);
-    console.log(data);
+    var allItems = [];
+    for(var i=0; i < keys.length; i++){
+      allItems.push({id: keys[i], name: AsyncStorage.getItem(keys[i])})
+    }
+    setItems(allItems);
   }
   
   useEffect(() => {
@@ -22,17 +25,29 @@ export default function listPage() {
 
   return (
     <View style={ styles.container }>
-      <Text>listPage</Text>
+      <SafeAreaView>
+        <FlatList 
+          data={items}
+          renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
+          keyExtractor={(item) => item.id}
+        />
+      </SafeAreaView>
       <Button Label="Go Home" onPress={async () => {router.replace('/')}} icon={"home"}/>
     </View>
   )
 
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'space-evenly',
+  },
+  item: {
+    padding: 20,
+    fontSize: 15,
+    marginTop: 5,
   }
 });
